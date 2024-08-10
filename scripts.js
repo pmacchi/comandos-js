df5203030673df51b6a452cd2cc1f4fa4b51e0fa
GMIRGOR\ext-pmacchi <pmacchi@hotmail.com.ar>
2024-08-09 23:05:12 -0300

    Operadores AND y OR

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+// Operador AND y OR
 
+//Operador AND &&
+
+var acuerdo = prompt('¿Acepta el acuerdo? Si o No');
+
+
+if (acuerdo === 'Si' && acuerdo === 'Si') {
+document.write('Aceptó los terminos y condiciones');
+} 
+else {
+document.write('No aceptó los terminos :( ');
+}
